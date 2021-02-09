5ddd4182ad8a3d831c3a9f66b4c78af3844d14b5
a.adamov <wof_4@mail.ru>
2021-02-09 08:37:24 +0300

    Updates

---
+++ precache-manifest.5dff663b1311a3250e09d739c366de75.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7202c583d09fed9dcaf15ecaf8f7e16c",
+    "url": "/avia/index.html"
+  },
+  {
+    "revision": "0e03f603e31ffda6f183",
+    "url": "/avia/static/css/main.4cc3a023.chunk.css"
+  },
+  {
+    "revision": "f5e5106b8882bc0c066b",
+    "url": "/avia/static/js/2.43278d53.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/avia/static/js/2.43278d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e03f603e31ffda6f183",
+    "url": "/avia/static/js/main.0cbd8fae.chunk.js"
+  },
+  {
+    "revision": "69f94631e2ce3126d245",
+    "url": "/avia/static/js/runtime-main.a7fa7afc.js"
+  },
+  {
+    "revision": "403829fb83f00afe3d7dd4fafb53b609",
+    "url": "/avia/static/media/AF.403829fb.png"
+  },
+  {
+    "revision": "a84ad2bcd4371d0c26f6fa99e5984dd8",
+    "url": "/avia/static/media/AY.a84ad2bc.png"
+  },
+  {
+    "revision": "f782607a42e455f5f261f78c41fbde33",
+    "url": "/avia/static/media/AZ.f782607a.png"
+  },
+  {
+    "revision": "57c9a5d4300c37a0a38411b314396cb0",
+    "url": "/avia/static/media/BT.57c9a5d4.png"
+  },
+  {
+    "revision": "4c512d935654f1de8d7a0320ef38e169",
+    "url": "/avia/static/media/KL.4c512d93.png"
+  },
+  {
+    "revision": "9b340336c5b800d43546b7fba8eb94f6",
+    "url": "/avia/static/media/LO.9b340336.png"
+  },
+  {
+    "revision": "9952a50c0f7dd613faf174ff9e55b62a",
+    "url": "/avia/static/media/PC.9952a50c.png"
+  },
+  {
+    "revision": "e7d35f642c4c3090c5f9a5b1ede6ec58",
+    "url": "/avia/static/media/Rt3jVfh.e7d35f64.png"
+  },
+  {
+    "revision": "90dd43430f06776b9b0b194854764b91",
+    "url": "/avia/static/media/SN.90dd4343.png"
+  },
+  {
+    "revision": "d38aab91f69e84c7764b3743441148f1",
+    "url": "/avia/static/media/SU.d38aab91.png"
+  },
+  {
+    "revision": "2990e9d467432465719cbb02af38f6f3",
+    "url": "/avia/static/media/TK.2990e9d4.png"
+  }
+]);
